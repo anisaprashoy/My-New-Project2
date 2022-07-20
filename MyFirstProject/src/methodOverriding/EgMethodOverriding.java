cfb7c88c4d33786e4da5f42d9bb39560dba10a22
prashoy <prashoy@192.168.1.4>
2022-07-20 12:23:40 +0530

    Commit1

---
+++ MyFirstProject/src/methodOverriding/EgMethodOverriding.java
@@ -0,0 +1,17 @@
+package methodOverriding;
+/*
+ Program to calculate discount If customer purchase clothes on Offseason, set discount 15% 
+ and on Onseason 40%  
+ Should use two classes, Onseason and Offseason  
+ Use two methods- discount(method name should be same)
+ */
+public class EgMethodOverriding 
+{
+	public static void main(String args[])
+	{
+		Offseason obj=new Offseason();
+		obj.display();
+	}
+}
+
+
